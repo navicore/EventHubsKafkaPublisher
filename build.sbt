8a56ed5945b9282909f2a4d94c1fec2ff8843484
Ed Sweeney <ed@table.co>
2017-01-07 21:54:01 -0800

    init

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-val sparkVersion = "1.6.1"
+val sparkVersion = "2.0.2"
 
 libraryDependencies ++=
     Seq(
@@ -14,7 +14,8 @@ libraryDependencies ++=
       "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
       "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided",
       "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
-      "org.apache.spark" % "spark-streaming-kafka_2.11" % "1.6.1",
+      "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.0.2",
+      "com.typesafe" % "config" % "1.2.1",
       "org.rogach" %% "scallop" % "2.0.2"
     )
 
@@ -33,11 +34,11 @@ assemblyMergeStrategy in assembly := {
 }
 
 // scala lint
-// lazy val compileScalastyle = taskKey[Unit]("compileScalastyle")
-// compileScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Compile).toTask("").value
-//
-// (compile in Compile) <<= (compile in Compile) dependsOn compileScalastyle
-//
-// lazy val testScalastyle = taskKey[Unit]("testScalastyle")
-// testScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Test).toTask("").value
-//
+lazy val compileScalastyle = taskKey[Unit]("compileScalastyle")
+compileScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Compile).toTask("").value
+
+(compile in Compile) <<= (compile in Compile) dependsOn compileScalastyle
+
+lazy val testScalastyle = taskKey[Unit]("testScalastyle")
+testScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Test).toTask("").value
+
--- src/main/scala/onextent/eventhubs/publisher/Main.scala
+++ src/main/scala/onextent/eventhubs/publisher/Main.scala
@@ -1,88 +1,57 @@
 package onextent.eventhubs.publisher
 
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
-import kafka.serializer.StringDecoder
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Milliseconds, StreamingContext}
-import org.rogach.scallop.ScallopConf
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 object Main extends Serializable with LazyLogging {
 
   def main(args: Array[String]): Unit = {
 
-    object Args extends ScallopConf(args) {
+    val config = ConfigFactory.load().getConfig("main")
 
-      val help = opt[Boolean]("holp", descr = "Get help.", required = true)
+    val sparkConfig = new SparkConf()
+    sparkConfig.set("spark.cores.max", "2")
+    val ssc = new StreamingContext(new SparkContext(sparkConfig), Seconds(config.getString("kafka.batchDuration").toInt))
 
-      // Kafka
-      val brokerList = opt[String]("broker-list", descr = "Kafka broker list", required = true)
-      val topic = opt[String]("input-topic", descr = "Kafka topic to read POS data from", required = true)
-      val offset = opt[String]("offset", descr = "'largest' for new, 'smallest' for all", required = false, default = Some("smallest"))
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> config.getString("kafka.brokerList"),
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> config.getString("kafka.consumerGroup"),
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
 
-      // Spark
-      val maxRate = opt[Int]("max-rate", descr = "Spark streaming max reads per second", default = Some(100))
-      val duration = opt[Long]("duration", descr = "Spark streaming batch duration, in milliseconds", default = Some(1000L))
-      val jobDuration = opt[Long]("job-duration", descr = "job will self-terminiate after N seconds", default = Some(30L))
-      val checkpointPath = opt[String]("checkpoint-path", descr = "Spark streaming checkpoint directory path", default = Some("/tmp/bits/spark"))
-      val appName = opt[String]("name", descr = "Spark application name", required = false, default = Some("KafkaTools"))
-      verify()
-    }
+    val topics = Array(config.getString("kafka.topic"))
 
-    if (Args.help()) {
-      println("hiya")
-      System.exit(0)
-    }
-    val mybrokers = Args.brokerList()
-    val mytopic = Args.topic()
-    logger.info(s"init config --> kafka brokers: $mybrokers topic: $mytopic")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
 
-    val processConf: SparkConf = new SparkConf(true)
-      .setAppName(Args.appName())
-      .setIfMissing("spark.master", "local[*]")
-      .set("spark.streaming.kafka.maxRatePerPartition", Args.maxRate().toString)
-
-    val ssc = new StreamingContext(processConf, Milliseconds(Args.duration()))
-
-    val sc = ssc.sparkContext
-    val sqlContext = new SQLContext(sc)
-
-    process(ssc, sqlContext,  Args.topic(), Args.brokerList())
+    stream.map(record => (record.key, record.value)).foreachRDD(rdd => rdd.foreach(o => {
+      println(s"key ${o._1} val: ${o._2}")
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+      //todo: ejs write the val (o._2) to event hubs!
+    }))
 
     ssc.start()
-    ssc.awaitTerminationOrTimeout(Args.jobDuration().toLong * 1000)
+    ssc.awaitTermination()
+
   }
-
-  def process(ssc: StreamingContext,  sqlContext: SQLContext, DataTopic: String, brokerList: String) : Unit = {
-    logger.info("process")
-    // Initialize stream of device data and processing
-    val dataStream = readStream(ssc, DataTopic, brokerList)
-    dataStream.foreachRDD { (rdd, time) =>
-      if (rdd.isEmpty()) {
-        logger.debug("no data for batch " + time)
-      } else {
-        logger.debug("-------- reading data ---------")
-
-        rdd.collect().foreach( json => {
-          logger.info("raw data:\n" + json)
-        })
-        //rawdata.show()
-
-        logger.debug("completed batch")
-      }
-    }
-  }
-
-  def readStream(ssc: StreamingContext, kafkaDataTopic: String, brokerList: String): DStream[String] = {
-    val kafkaParams = Map(
-      ("metadata.broker.list", brokerList),
-      ("auto.offset.reset", "smallest")
-    )
-    val raw = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, Set(kafkaDataTopic))
-    raw.map(_._2)
-  }
-
 }
 
